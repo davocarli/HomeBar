8a36bd0ee68e7eb24f50e2084922efbe244d111b
David Carli-Arnold <davocarli@gmail.com>
2021-02-14 16:30:14 -0800

    Add Registration Validator

---
+++ src/main/java/app/davocarli/homebar/validators/RegistrationValidator.java
@@ -0,0 +1,61 @@
+package app.davocarli.homebar.validators;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import app.davocarli.homebar.models.User;
+import app.davocarli.homebar.services.UserService;
+
+@Component
+public class RegistrationValidator implements Validator {
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return User.class.equals(clazz);
+	}
+	
+	public void validate(Object target, Errors errors, UserService service) {
+		User user = (User)target;
+		
+		// Passwords don't match
+		if (!user.getPassword().equals(user.getPasswordConfirmation())) {
+			errors.rejectValue("passwordConfirmation", "MATCH");
+		} 
+		
+		// Password is too short
+		if (user.getPassword().length() < 8) {
+			errors.rejectValue("password", "LENGTH");
+		} 
+		
+		// First Name is blank
+		if (user.getFirstName().length() < 1) {
+			errors.rejectValue("firstName", "EMPTY");
+		}
+		
+		// Last Name is blank
+		if (user.getLastName().length() < 1) {
+			errors.rejectValue("lastName", "EMPTY");
+		}
+		
+		// Username is too short
+		if (user.getUsername().length() < 4) {
+			errors.rejectValue("username", "LENGTH");
+		}
+		
+		// Username is taken
+		if (service != null && service.findByUsername(user.getUsername()) != null) {
+			errors.rejectValue("username", "TAKEN");
+		}
+		
+		// Email is taken
+		if (service != null && service.findByEmail(user.getEmail()) != null) {
+			errors.rejectValue("email", "TAKEN");
+		}
+	}
+	
+	@Override
+	public void validate(Object target, Errors errors) {
+		this.validate(target, errors, null);
+	}
+}
